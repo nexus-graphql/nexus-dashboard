e69fa9aa3d8921cbb30eca35ccd8a33cabff8dc4
Matt Malane <mattmalane@icloud.com>
2022-11-03 13:31:08 -0700

    stylistic changes

--- frontend/src/components/Graphiql.js
+++ frontend/src/components/Graphiql.js
@@ -4,7 +4,12 @@ const Graphiql = () => {
   return (
     <>
       <Sidebar />
-      <div className="relative md:ml-64 bg-blueGray-100 h-screen">
+      <div className="relative md:ml-64 h-screen">
+        <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
+          <h1 class="text-indigo text-2xl uppercase hidden lg:inline-block font-semibold">
+            Local Development Server
+          </h1>
+        </div>
         <iframe
           title="GraphiQL"
           width="100%"
